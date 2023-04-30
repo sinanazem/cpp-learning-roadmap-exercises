313bf4fefbb0bfc0f65147d9e8b3946a0351b487
sinanazem <sinanaazem@gmail.com>
2023-04-30 10:42:17 +0430

    update & add cpp code

---
+++ code/nested-for-loop/nested_for_loop.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+    for(i=1; i<=2; i++){
+        for (j=1; j<=5; j++){
+            cout << "*" ;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
